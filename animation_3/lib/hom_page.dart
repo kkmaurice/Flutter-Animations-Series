9039fb7914311a4e31f9be6686f77cd67c6dd9ac
kkmaurice <adonyomaurice@gmail.com>
2023-09-05 07:15:58 +0300

    started animation three

---
+++ animation_3/lib/hom_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
